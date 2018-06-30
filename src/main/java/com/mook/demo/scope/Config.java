4362890b3836ed489c064ae8c7baa9c85ea10024
maojunkai <maojunkai@youzan.com>
2018-06-30 17:17:51 +0800

    bean的作用域

---
+++ src/main/java/com/mook/demo/scope/Config.java
@@ -0,0 +1,20 @@
+package com.mook.demo.scope;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+/**
+ * @Author: maojunkai
+ * @Date: 2018/6/30 下午5:07
+ * @Description:
+ */
+@Configuration
+public class Config {
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public Notepad notepad() {
+        return new Notepad();
+    }
+}
